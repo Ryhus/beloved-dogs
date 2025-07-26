ccfc271cc4584626f359cd0f5b180f5f74e659d0
Ryhus <ryhusyevhen@gmail.com>
2025-07-26 16:31:21 +0200

    feat(about page): add about page

--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import PageNotFound from '../pages/PageNotFound.tsx/PageNotFound';
-
+import About from '../pages/About/About';
 const router = createBrowserRouter([
   {
     path: '/',
     Component: Home,
+  },
+  {
+    path: '/about',
+    Component: About,
   },
   { path: '*', Component: PageNotFound },
 ]);
